5f7986f998edc2a5e113af94ded3625b2eded930
Alana Depaz <alanadepaz@fb.com>
2022-06-16 20:06:27 -0700

    Implemented ability to sign up new users from within app.

--- app/src/main/java/com/example/instagram_app/LoginActivity.java
+++ app/src/main/java/com/example/instagram_app/LoginActivity.java
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
     private ImageView ivIcon;
 
     @Override
@@ -35,6 +36,8 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +45,14 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick sign up button");
+                goSignUpActivity();
             }
         });
     }
@@ -64,6 +75,12 @@ public class LoginActivity extends AppCompatActivity {
 
     private void goMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();   // Finishing login activity once we've done the navigation
+    }
+
+    private void goSignUpActivity() {
+        Intent i = new Intent(this, SignUpActivity.class);
         startActivity(i);
         finish();   // Finishing login activity once we've done the navigation
     }
